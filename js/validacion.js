db6901a636fd6155dec188024530795a398146b7
Santiaguitto <santiago.acunia21@gmail.com>
2023-08-09 19:10:02 -0300

    passwords iguales y validacion

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,30 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+document.addEventListener("DOMContentLoaded", function() {
+    const regBtn = document.getElementById("regBtn");
+    
+    regBtn.addEventListener("click", function() {
+        const password1 = document.getElementById("password1").value;
+        const password2 = document.getElementById("password2").value;
+
+       
+
+        if (password1.length < 6) {
+            showAlertError();
+            return;
+        }
+
+        if (password1 !== password2) {
+            showAlertError();
+            return;
+        }
+
+        const terminosCheckbox = document.getElementById("terminos");
+        if (!terminosCheckbox.checked) {
+            showAlertError();
+            return;
+        }
+        showAlertSuccess();
+    });
+});
